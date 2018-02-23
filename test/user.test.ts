fa3f28a32561fe4416dfd14cd01504c51ee2a3e2
Roberto Sousa <beto.sousa22@gmail.com>
2018-02-23 17:58:23 +0000

    test if the user can add a payment method

--- test/user.test.ts
+++ test/user.test.ts
@@ -109,3 +109,148 @@ describe("test the routes that required authentication", () => {
 	});
 
 });
+
+
+
+describe("add a new payment method to the user", () => {
+
+	/*
+	clean up the test db before and after the
+	test just to be sure that the tests are really
+	independent from each others
+	*/
+
+	beforeEach(() => {
+		if (mongoose.connection && mongoose.connection.db) {
+			mongoose.connection.db.dropDatabase();
+		}
+	});
+	afterAll(() => {
+		if (mongoose.connection && mongoose.connection.db) {
+			mongoose.connection.db.dropDatabase();
+		}
+	});
+
+	it("should be able to successfully add a new payment method to the user", (done) => {
+		createFakeUser((err: Error) => {
+			request(app).post("/auth/signin")
+			.set("Accept", "application/json")
+			.set("Content-Type", "application/json")
+			.send({
+				"email": "placeholder@mail.com",
+				"password": "123"
+			})
+			.expect(200)
+			.expect("Content-Type", /json/)
+			.expect("set-cookie", /connect.sid/)
+			.end(function (err, res) {
+				if (err) {
+					done(err);
+					return;
+				}
+				chai.expect(res.body.message).to.equal("login with success");
+				request(app).post("/user/payment/source")
+				.set("Accept", "application/json")
+				.set("Cookie", res.header["set-cookie"])
+				.set("Content-Type", "application/json")
+				.send({
+					"source": "tok_mastercard"
+				})
+				.expect(200)
+				.expect("Content-Type", /json/)
+				.end(function (err, res) {
+					if (err) {
+						done(err);
+						return;
+					}
+					chai.expect(res.body.message).to.equal("payment method added");
+					chai.expect(res.body.data).to.exist;
+					chai.expect(res.body.data.id).to.exist;
+					done();
+					return;
+				});
+			});
+		});
+	});
+
+	it("should get the 400 error if no source provided", (done) => {
+		createFakeUser((err: Error) => {
+			request(app).post("/auth/signin")
+			.set("Accept", "application/json")
+			.set("Content-Type", "application/json")
+			.send({
+				"email": "placeholder@mail.com",
+				"password": "123"
+			})
+			.expect(200)
+			.expect("Content-Type", /json/)
+			.expect("set-cookie", /connect.sid/)
+			.end(function (err, res) {
+				if (err) {
+					done(err);
+					return;
+				}
+				chai.expect(res.body.message).to.equal("login with success");
+				request(app).post("/user/payment/source")
+				.set("Accept", "application/json")
+				.set("Cookie", res.header["set-cookie"])
+				.set("Content-Type", "application/json")
+				.send({
+				})
+				.expect(400)
+				.expect("Content-Type", /json/)
+				.end(function (err, res) {
+					if (err) {
+						done(err);
+						return;
+					}
+					chai.expect(res.body.message).to.equal("missing source");
+					chai.expect(res.body.data).to.not.exist;
+					done();
+					return;
+				});
+			});
+		});
+	});
+
+	it("should get the 500 error if the source provided is not valid", (done) => {
+		createFakeUser((err: Error) => {
+			request(app).post("/auth/signin")
+			.set("Accept", "application/json")
+			.set("Content-Type", "application/json")
+			.send({
+				"email": "placeholder@mail.com",
+				"password": "123"
+			})
+			.expect(200)
+			.expect("Content-Type", /json/)
+			.expect("set-cookie", /connect.sid/)
+			.end(function (err, res) {
+				if (err) {
+					done(err);
+					return;
+				}
+				chai.expect(res.body.message).to.equal("login with success");
+				request(app).post("/user/payment/source")
+				.set("Accept", "application/json")
+				.set("Cookie", res.header["set-cookie"])
+				.set("Content-Type", "application/json")
+				.send({
+					"source": "noValid"
+				})
+				.expect(500)
+				.expect("Content-Type", /json/)
+				.end(function (err, res) {
+					if (err) {
+						done(err);
+						return;
+					}
+					chai.expect(res.body.message).to.equal("failed");
+					chai.expect(res.body.data).to.not.exist;
+					done();
+					return;
+				});
+			});
+		});
+	});
+});
