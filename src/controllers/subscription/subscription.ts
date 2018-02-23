0b46beaa56d04542de482d2a5735fdf4e1199d21
Roberto Sousa <beto.sousa22@gmail.com>
2018-02-23 08:55:06 +0000

    clean up the code and build the user subscription

--- src/controllers/subscription/subscription.ts
+++ src/controllers/subscription/subscription.ts
@@ -14,22 +14,6 @@ import { stripeSubscription, StripePlan, StripeSubscription, stripePlan } from "
 
 const stripe = require("stripe")(process.env.STRIPE_SDK);
 
-const planMonth = stripe.plans.create({
-	product: {name: "sample Product"},
-	currency: "usd",
-	interval: "month",
-	nickname: "sample Monthly",
-	amount: 10,
-});
-
-const planYear = stripe.plans.create({
-	product: {name: "sample Product"},
-	currency: "usd",
-	interval: "year",
-	nickname: "sample Year",
-	amount: 100,
-});
-
 /*
 	GET /subscription/list
 	get the subscriptions of the user
@@ -38,7 +22,10 @@ export const list = (req: Request, res: Response, next: NextFunction) => {
 	console.log("req-->", req); // roberto
 	console.log("session-->", req.session); // roberto
 	console.log("user-->", req.user); // roberto
-	res.status(200).json({message: "sample sample", error: undefined, data: undefined});
+	/*
+	TODO: get the list of user subscrition
+	*/
+	res.status(200).json({message: "success", error: undefined, data: undefined});
 	return;
 };
 
@@ -49,17 +36,68 @@ export const list = (req: Request, res: Response, next: NextFunction) => {
 */
 export const newSub = (req: Request, res: Response, next: NextFunction) => {
 	console.log("user-->", req.user); // roberto
-	stripe.subscriptions.create({
-		customer: req.user.stripeId,
-		items: [{plan: "plan_CNFLj9SXobukeW"}],
-	}, (err: Error, newSubscription: any) => {
+	if (!req.body.plan) {
+		res.status(400).json({message: "missing required params", error: undefined, data: undefined});
+		return;
+	}
+	StripeSubscription.find({customer: req.user.stripeId, plan: req.body.plan}, (err: Error, subFind: Array<stripeSubscription>) => {
 		if (err) {
-			console.log("err-->", err); // roberto
-			res.status(500).json({message: undefined, error: err.message, data: undefined});
+			res.status(500).json({message: "failed", error: err.message, data: undefined});
 			return;
 		}
-		console.log("newSubscription-->", newSubscription); // roberto
-		res.status(200).json({message: "sample sample", error: undefined, data: userSalt(req.user)});
+
+		// doesn't allow the user to subscribe twice to the same plan (repeated subscriptions)
+		if (subFind.length > 0) {
+			res.status(200).json({message: "user already subscribed to this plan", error: undefined, data: subFind});
+			return;
+		}
+
+		stripe.subscriptions.create({
+			customer: req.user.stripeId,
+			items: [{plan: req.body.plan}],
+		}, (err: Error, newSubscription: stripeSubscription) => {
+			if (err) {
+				/*
+				TODO: need to build teh user payment plan
+				*/
+				res.status(500).json({message: "failed", error: err.message, data: undefined});
+				return;
+			}
+			const newSubsData  = new StripeSubscription({
+				id: newSubscription.id,
+				object: newSubscription.object,
+				billing: newSubscription.billing,
+				billing_cycle_anchor: newSubscription.billing_cycle_anchor,
+				cancel_at_period_end: newSubscription.cancel_at_period_end,
+				canceled_at: newSubscription.canceled_at,
+				created: newSubscription.created,
+				customer: newSubscription.customer,
+				planId: newSubscription.plan.id,
+				start: newSubscription.start,
+				status: newSubscription.status,
+				trial_end: newSubscription.trial_end ? newSubscription.trial_end : undefined,
+				trial_start: newSubscription.trial_start ? newSubscription.trial_start : undefined
+			});
+			newSubsData.save();
+			res.status(200).json({message: "success", error: undefined, data: newSubscription});
+			return;
+		});
+	});
+};
+
+
+
+/*
+	GET /subscription/plan/list
+	get the list of existing plans
+*/
+export const planList = (req: Request, res: Response, next: NextFunction) => {
+	StripePlan.find({}, (err: Error, plansListRes: Array<stripePlan>) => {
+		if (err) {
+			res.status(500).json({message: "error", error: err.message, data: undefined});
+			return;
+		}
+		res.status(200).json({message: "success", error: undefined, data: plansListRes});
 		return;
 	});
 };
@@ -67,7 +105,8 @@ export const newSub = (req: Request, res: Response, next: NextFunction) => {
 
 /*
 	POST /subscription/newplan
-	build a new user subscription
+	build a new  plan
+	this works for admins only
 */
 export const newPlan = (req: Request, res: Response, next: NextFunction) => {
 
@@ -83,11 +122,9 @@ export const newPlan = (req: Request, res: Response, next: NextFunction) => {
 		amount: req.body.amount,
 	}, (err: Error, planNew: stripePlan) => {
 		if (err) {
-			console.log("err-->", err); // roberto
 			res.status(500).json({message: undefined, error: err.message, data: undefined});
 			return;
 		}
-		console.log("plan-->", planNew); // roberto
 		// build the plan
 		const plan = new StripePlan({
 			id: planNew.id,
@@ -101,6 +138,7 @@ export const newPlan = (req: Request, res: Response, next: NextFunction) => {
 			trial_period_days: planNew.trial_period_days ? planNew.trial_period_days : 0,
 			name: planNew.name
 		});
+
 		// save it on the db
 		plan.save();
 		res.status(200).json({message: "success", error: undefined, data: plan});
