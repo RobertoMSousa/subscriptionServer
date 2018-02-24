cb5b3c0cc042f7e01937178e6d5adbe0652c074a
Roberto Sousa <beto.sousa22@gmail.com>
2018-02-24 09:03:20 +0000

    dry frunction to create plan

--- test/subscription.test.ts
+++ test/subscription.test.ts
@@ -13,6 +13,9 @@ const chai = require("chai");
 const expect = chai.expect;
 
 
+/*
+	create a fake user
+*/
 function createFakeUser(callback: (err: Error) => void): void {
 	request(app).post("/auth/signup")
 	.set("Accept", "application/json")
@@ -35,6 +38,9 @@ function createFakeUser(callback: (err: Error) => void): void {
 }
 
 
+/*
+	create a fake admin
+*/
 function createFakeAdmin(callback: (err: Error) => void): void {
 	request(app).post("/auth/signup")
 	.set("Accept", "application/json")
@@ -59,6 +65,44 @@ function createFakeAdmin(callback: (err: Error) => void): void {
 	});
 }
 
+/*
+function that creates a new plan
+*/
+function createPlan(cookie: string, name: string, nickname: string, amount: number, currency: string, interval: string, callback: (err: Error, plan: stripePlan) => void): void {
+	request(app).post("/subscription/newplan")
+	.set("Accept", "application/json")
+	.set("Cookie", cookie)
+	.set("Content-Type", "application/json")
+	.send({
+		"name": name,
+		"nickname": nickname,
+		"amount": amount,
+		"currency": currency,
+		"interval": interval
+	})
+	.expect(200)
+	.end(function (err, res) {
+		if (err) {
+			callback(err, undefined);
+			return;
+		}
+		chai.expect(res.body.message).to.equal("success");
+		chai.expect(res.body.data).to.exist;
+		chai.expect(res.body.error).to.not.exist;
+		chai.expect(res.body.data).to.exist;
+		chai.expect(res.body.data.id).to.exist;
+		chai.expect(res.body.data._id).to.exist;
+		chai.expect(res.body.data.created).to.exist;
+		chai.expect(res.body.data.trial_period_days).to.equal(0);
+		chai.expect(res.body.data.amount).to.equal(amount);
+		chai.expect(res.body.data.name).to.equal(name);
+		chai.expect(res.body.data.currency).to.equal(currency);
+		chai.expect(res.body.data.nickname).to.equal(nickname);
+		chai.expect(res.body.data.interval).to.equal(interval);
+		callback(undefined, res.body.data);
+		return;
+	});
+}
 
 
 describe("create plans", () => {
